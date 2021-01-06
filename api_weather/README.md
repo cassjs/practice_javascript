60ba13723f1f5eb182661cdf1351f8d1ee00eda7
cassjsdev <cassjes.m@gmail.com>
2021-01-06 03:05:11 -0800

    project update

--- api_weather/README.md
+++ api_weather/README.md
@@ -1,14 +1,37 @@
-# Project - Weather Forecast API
+# Project - Weather Forecast
 **Author:**     Jessica Cassidy
 
-## Description: 
-Project in progress. 
+## Status: 🚧 Project in Progress 🚧
+**Last Updated:** 1/6
+
+**Current Issues:**
+- [ ] Trying to assign city and API key to variables and pass to apiUrl, get error 401 but when entered directly into apiUrl, data runs fine. My plan is to eventually hide the API. For now, I am removing API key before commiting code to public repo on GitHub. 
+- [ ] Search function not working. Whatever city the user inputs, it is stored in the inputCity variable. I then need to pass this info to the name variable when JSON object 'name' is destructured?
+
+**Future Updates:**
+- [ ] Hide API key - My plan is to eventually hide the API key after learning how. For now, I am removing API key before commiting code to public repo on GitHub.
+- [ ] For the location search function, I want to implement the a location API via a dropdown menu, possibly use Google Places API. 
+- [ ] Check if there is an option to add data on expected rainfall for the day? If not, switch to another API? 
+- [ ] Add text warnings for incoming rainfall within the next hour. 
+- [ ] Add styling to application.
+
+## Description
+The Weather Forecast application generates current conditions based on a city search. The weather data is sourced via the [Weather API](https://www.weatherapi.com/).
 
 **Input:**      
-
+Enter City
 
 **Output:**     
+Location
+- City:
+- Region:
 
-
+Current Conditions
+- Temp (f):
+- Condition:
+- Precipitation:
+- Wind (mph):
+- Humidity:
+- Last Updated:
 ## Program Output Example:
-
+![](images/screenshot_program-output_1.png)
--- api_weather/fetch.js
+++ api_weather/fetch.js
@@ -1,18 +1,43 @@
 async function getData() {
-    //Source: Open Weather API - https://openweathermap.org/api/one-call-api
-    let city_id = 2562;
-    const api_key = ''; //remove key before loading to gitHub, need to hide key
-    const apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=${city_id}&appid=${api_key}';  //switch out to one-call API
+    //Source: Weather API - https://www.weatherapi.com/docs/
+    const apiKey = ''; // Removed - need to hide key
+    const inputCity = document.getElementById("citySearch").placeholder;
+    const apiUrl = 'http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${inputCity}';
+    // const apiUrl = 'http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Portland';
+
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
-    // const { text } = data;
 
-    // document.getElementById('dataResult').textContent = text;
+    //*******CONSOLE TESTING*******//
+    console.log(data); // console test - logs all JSON data from apiUrl
+    // console.log(data.location.name); //Example: [City] Portland
+    // console.log(data.location.region); //Example: [State] Oregon
+    // console.log(data.current.temp_f); // Example: [Temp] ##
+    // console.log(data.current.condition.text); //Example: [Condition] Overcast
+    // console.log(data.current.precip_in); //Example: [Precipitation] ##
+    // console.log(data.current.wind_mph); //Example: [Wind (mph)] ##
+    // console.log(data.current.humidity); //Example: [Humidity] ##
+    // console.log(data.current.last_updated); //Example: [Last Updated] 2021-01-05 20.30
+    
+    const { name, region } = data.location; // destructuring JS objects
+    // console.log(name , region); // console test after destructuring
 
-    // btn.addEventListener("click", getData)
+    const { temp_f, precip_in, wind_mph, humidity, last_updated } = data.current; // destructuring JS objects
+    // console.log(temp_f,= condition, precip_in, wind_mph, humidity, last_updated); // console test after destructuring
 
-    // data.innerHTML = dataResult;
+    const { text } = data.current.condition; 
+    // console.log(text); // console test after destructuring 
+
+    document.getElementById('citySearch').textContent = name;
+    document.getElementById('region').textContent = region;
+    document.getElementById('temp').textContent = temp_f;
+    document.getElementById('condition').textContent = text;
+    document.getElementById('precipitation').textContent = precip_in;
+    document.getElementById('wind').textContent = wind_mph;
+    document.getElementById('humidity').textContent = humidity;
+    document.getElementById('updated').textContent = last_updated;
+
+    bttn.addEventListener("click", getData); // button
 };
 
 getData();
